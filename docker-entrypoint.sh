1614d2e30a8ef2a293de38291270ae4c560c072f
Abhishek Dubey <8484451+adubey14@users.noreply.github.com>
2021-11-11 13:51:27 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ $# -eq 0 ]
   then
-    jupyter lab --ip=0.0.0.0 --port=2222 --NotebookApp.token='riaps' --no-browser &
+    jupyter lab --ip=0.0.0.0 --port=2222 --NotebookApp.token='riaps' --no-browser --allow-true &
     code-server --auth none   --bind-addr=0.0.0.0:8443 --extensions-dir /extensions  /code
   else
     exec "$@"
